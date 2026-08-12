05f51242e66743d686a3bae281efbe91e4b6f778
Shreya Chopra <chopra.shreya03@gmail.com>
2026-08-13 00:02:33 +0530

    add resume PDF viewer

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import resumePDF from "./assets/Yash_Gupta_Resume.pdf";
+// import React from "react";
+import resumePDF from "./assets/Shreya_Chopra_Resume.pdf";
 
 const Resume = () => {
   return (
     <div className="fixed inset-0 w-full h-full overflow-hidden">
       <object data={resumePDF} type="application/pdf" className="w-full h-full">
         <p className="p-4 text-center">
-          It appears your browser doesn't support embedded PDFs. You can{" "}
+          It appears your browser does not support embedded PDFs. You can{" "}
           <a
             href={resumePDF}
             className="text-blue-500 hover:text-blue-700 underline"
